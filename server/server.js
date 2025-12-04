8e8e6175a73ecee5055fd54a2c4e53bb70f2d3cf
Shree <shreyaray5829@gmail.com>
2025-12-04 21:41:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-jghy.vercel.app/", // allow React frontend
+    origin: "https://chat-app-jghy.vercel.app", // allow React frontend
     methods: ["GET", "POST"]
   }
 });
